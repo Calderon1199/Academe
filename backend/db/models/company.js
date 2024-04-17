cad664de09b347561957388e18d0479a76ebc8ee
Daniel Calderon <danielcalderon1199@gmail.com>
2024-04-16 23:07:43 -0700

    Adding company table to database with seeder files for 3 companies

---
+++ backend/db/models/company.js
@@ -0,0 +1,80 @@
+'use strict';
+const {
+    Model,
+    Validator
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Company extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Company.init({
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phoneNumber: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [10, 10]
+            }
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [0, 100]
+            }
+        },
+        state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [0, 2]
+            }
+        },
+        zipcode: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [5, 10]
+            }
+        },
+        logo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "https://img.freepik.com/free-vector/colorful-letter-gradient-logo-design_474888-2309.jpg"
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            }
+        },
+        hashedPassword: {
+            type: DataTypes.STRING.BINARY,
+            allowNull: false,
+            validate: {
+                len: [60, 60]
+            }
+        },
+    }, {
+        sequelize,
+        modelName: 'Company',
+        defaultScope: {
+            attributes: {
+                exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
+            }
+        }
+    });
+    return Company;
+};
