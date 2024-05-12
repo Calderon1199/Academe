496b4a3c71a2d5075302007e65c1588bc41d8228
Daniel Calderon <danielcalderon1199@gmail.com>
2024-05-11 17:53:55 -0700

    Adding skeleton for side bar with 2 buttons sections

--- frontend/src/components/HomeNav/HomeNav.jsx
+++ frontend/src/components/HomeNav/HomeNav.jsx
@@ -6,7 +6,7 @@ function HomeNav(props) {
     const user = useSelector(state => state.session.user);
 
     return (
-        <div>
+        <div className='sidebar'>
             <div className='nav-bar-intro'>
                 <img src='../../public/assets/letter-t.png' className='team-icon'></img>
                 <div>
@@ -15,9 +15,25 @@ function HomeNav(props) {
                 </div>
                 <i class="fa-solid fa-circle-chevron-left"></i>
             </div>
-            <div>
-                <img></img>
+            <div className='user-header'>
+                <img src={user?.logo} style={{width: '50px', height: '50px'}}></img>
                 <h5>{user?.name}</h5>
+            </div>
+            <div className='first-section-buttons'>
+                <button><i class="fa-solid fa-chart-line"></i> My Activity</button>
+                <button><i class="fa-regular fa-user"></i> My Profile</button>
+            </div>
+            <div className='pages'>
+                <div className='pages-header'>
+                    <h3>Pages</h3>
+                    <i class="fa-solid fa-chevron-up"></i>
+                </div>
+                <div className='second-section-buttons'>
+                    <button><i class="fa-solid fa-house"></i> Dashboard</button>
+                    <button><i class="fa-regular fa-folder"></i> Folders</button>
+                    <button><i class="fa-regular fa-message"></i> Chat</button>
+                    <button><i class="fa-regular fa-calendar-days"></i> Calendar</button>
+                </div>
             </div>
         </div>
     );
