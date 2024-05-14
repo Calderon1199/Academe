3c36867b17526412c7cf395b32c36660cec31868
Daniel Calderon <danielcalderon1199@gmail.com>
2024-05-13 23:56:25 -0700

    Adding mini sidebar and completed sidebar minus the folder and routes

--- frontend/src/components/HomeNav/HomeNav.jsx
+++ frontend/src/components/HomeNav/HomeNav.jsx
@@ -1,53 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './HomeNav.css';
 import { useSelector } from 'react-redux';
+import SideBarMini from './SideBarMini';
 
 function HomeNav(props) {
     const user = useSelector(state => state.session.user);
+    const [closePages, setClosePages] = useState(false);
+    const [closeLabels, setCloseLabels] = useState(false);
+    const [closeMenu, setCloseMenu] = useState(true);
+
+
 
     return (
-        <div className='sidebar'>
-            <div className='nav-bar-intro'>
-                <img src='../../public/assets/letter-t.png' className='team-icon'></img>
+        <>
+        {closeMenu ? (
+            <SideBarMini user={user}/>
+        ): (
+            <div className='sidebar'>
+                <div className='nav-bar-intro'>
+                    <div className='profile-intro'>
+                    <img src='../../public/assets/letter-t.png' className='team-icon'></img>
+                    <div className='profile-names'>
+                        <h4>Team</h4>
+                        <p>Company Management</p>
+                    </div>
+                    </div>
+                    {closeMenu ? (
+                        <i class="fa-solid fa-angles-right close-chevron" onClick={() => setCloseMenu(!closeMenu)}></i>
+                    ): (
+                        <i class="fa-solid fa-angles-left close-chevron" onClick={() => setCloseMenu(!closeMenu)}></i>
+                    )}
+                </div>
+                <div className='user-header'>
+                    <img src={user?.logo} style={{width: '40px', height: '40px', borderRadius: '40px', objectFit: 'cover'}}></img>
+                    <h5>{user?.name}</h5>
+                </div>
+                <div className='first-section-buttons'>
+                    <button><i class="fa-solid fa-chart-line"></i> Activity</button>
+                    <button><i class="fa-regular fa-user"></i> My Profile</button>
+                </div>
+                <div className='pages'>
+                    <div className='pages-header'>
+                        <h4>PAGES</h4>
+                        {closePages ? (
+                            <i class="fa-solid fa-chevron-down close-chevron" onClick={() => setClosePages(!closePages)}></i>
+                        ): (
+                            <i class="fa-solid fa-chevron-up close-chevron" onClick={() => setClosePages(!closePages)}></i>
+                        )}
+                    </div>
+                    {!closePages && (
+                        <div className='second-section-buttons'>
+                            <button><i class="fa-solid fa-house"></i> Dashboard</button>
+                            <button><i class="fa-regular fa-folder"></i> Folders</button>
+                            <button><i class="fa-regular fa-message"></i> Chat</button>
+                            <button><i class="fa-regular fa-calendar-days"></i> Calendar</button>
+                        </div>
+                    )}
+                </div>
                 <div>
-                    <h3>Team</h3>
-                    <h3>Assist</h3>
-                </div>
-                <i class="fa-solid fa-circle-chevron-left"></i>
-            </div>
-            <div className='user-header'>
-                <img src={user?.logo} style={{width: '50px', height: '50px'}}></img>
-                <h5>{user?.name}</h5>
-            </div>
-            <div className='first-section-buttons'>
-                <button><i class="fa-solid fa-chart-line"></i> My Activity</button>
-                <button><i class="fa-regular fa-user"></i> My Profile</button>
-            </div>
-            <div className='pages'>
-                <div className='pages-header'>
-                    <h3>Pages</h3>
-                    <i class="fa-solid fa-chevron-up"></i>
-                </div>
-                <div className='second-section-buttons'>
-                    <button><i class="fa-solid fa-house"></i> Dashboard</button>
-                    <button><i class="fa-regular fa-folder"></i> Folders</button>
-                    <button><i class="fa-regular fa-message"></i> Chat</button>
-                    <button><i class="fa-regular fa-calendar-days"></i> Calendar</button>
+                    <div className='labels-header'>
+                        <h4>LABELS</h4>
+                        {closeLabels ? (
+                            <i class="fa-solid fa-chevron-down close-chevron" onClick={() => setCloseLabels(!closeLabels)}></i>
+                        ): (
+                            <i class="fa-solid fa-chevron-up close-chevron" onClick={() => setCloseLabels(!closeLabels)}></i>
+                        )}
+                    </div>
+                    {!closeLabels && (
+                        <div className='second-section-buttons'>
+                            <button><i class="fa-solid fa-bookmark bk-high"></i> Custom</button>
+                            <button><i class="fa-solid fa-bookmark bk-medium"></i> Custom</button>
+                            <button><i class="fa-solid fa-bookmark bk-low"></i> Custom</button>
+                            <button><i class="fa-solid fa-bookmark bk-on-hold"></i> Custom</button>
+                        </div>
+                    )}
                 </div>
             </div>
-            <div>
-                <div>
-                    <h3>Labels</h3>
-                    <i class="fa-solid fa-chevron-up"></i>
-                </div>
-                <div className='second-section-buttons'>
-                    <button><i class="fa-solid fa-bookmark"></i> Custom</button>
-                    <button><i class="fa-solid fa-bookmark"></i> Custom</button>
-                    <button><i class="fa-solid fa-bookmark"></i> Custom</button>
-                    <button><i class="fa-solid fa-bookmark"></i> Cusotm</button>
-                </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
 
