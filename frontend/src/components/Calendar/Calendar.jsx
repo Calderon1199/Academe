dd4177da34f15ef08f5e20191ec45defbaa0cb0f
Daniel Calderon <danielcalderon1199@gmail.com>
2024-06-29 17:29:02 -0700

    Adding weekly calendar dates header for weekly view

--- frontend/src/components/Calendar/Calendar.jsx
+++ frontend/src/components/Calendar/Calendar.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calendar.css';
 import HomeNav from '../HomeNav';
+import CalendarWeek from './CalendarWeek';
 
 function Calendar() {
     let date = new Date();
+    const [selected, setSelected] = useState('');
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     const getDaysInCurrentMonth = () => {
@@ -30,43 +32,48 @@ function Calendar() {
     return (
         <div className="calendar-container">
             <HomeNav />
-            <div className="calendar-content">
-                <div className="calendar-header">
-                    <h2>{`${months[date.getMonth()]}, ${date.getFullYear()}`}</h2>
-                    <div className="calendar-controls1">
-                        <button>Month</button>
-                        <button>Week</button>
-                        <button>Day</button>
-                    </div>
-                    <div className="calendar-controls2">
-                        <button><i className="fa-solid fa-chevron-left"></i></button>
-                        <span>Today</span>
-                        <button><i className="fa-solid fa-chevron-right"></i></button>
-                    </div>
-                </div>
-                <div className="calendar-day-names">
-                    {['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'].map(day => (
-                        <div className="day-name" key={day}>{day}</div>
-                    ))}
-                </div>
-                <div className="calendar-grid">
-                    <div className="calendar-days">
-                        {Array(firstDayIndex).fill(null).map((_, index) => (
-                            <div key={`empty-${index}`} className="calendar-day empty">
-                                {daysInPreviousMonth - firstDayIndex + index + 1}
-                            </div>
-                        ))}
-                        {days.map(day => (
-                            <div
-                                key={day}
-                                className={`calendar-day ${day === currentDay ? 'current-day' : ''}`}
-                            >
-                                {day}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
+            {selected === 'week' && (
+              < CalendarWeek />
+            )}
+            {selected === 'month' && (
+              <div className="calendar-content">
+                  <div className="calendar-header">
+                      <h2>{`${months[date.getMonth()]}, ${date.getFullYear()}`}</h2>
+                      <div className="calendar-controls1">
+                          <button>Month</button>
+                          <button onClick={() => setSelected('week')}>Week</button>
+                          <button>Day</button>
+                      </div>
+                      <div className="calendar-controls2">
+                          <button><i className="fa-solid fa-chevron-left"></i></button>
+                          <span>Today</span>
+                          <button><i className="fa-solid fa-chevron-right"></i></button>
+                      </div>
+                  </div>
+                  <div className="calendar-day-names">
+                      {['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'].map(day => (
+                          <div className="day-name" key={day}>{day}</div>
+                      ))}
+                  </div>
+                  <div className="calendar-grid">
+                      <div className="calendar-days">
+                          {Array(firstDayIndex).fill(null).map((_, index) => (
+                              <div key={`empty-${index}`} className="calendar-day empty">
+                                  {daysInPreviousMonth - firstDayIndex + index + 1}
+                              </div>
+                          ))}
+                          {days.map(day => (
+                              <div
+                                  key={day}
+                                  className={`calendar-day ${day === currentDay ? 'current-day' : ''}`}
+                              >
+                                  {day}
+                              </div>
+                          ))}
+                      </div>
+                  </div>
+              </div>
+            )}
         </div>
     );
 }
